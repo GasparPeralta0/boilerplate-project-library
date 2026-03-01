e0bb7ea7169a8a37a7252785542befb122810198
Gaspi <gaspar_peralta09@hotmail.com>
2026-03-01 02:55:36 +0100

    chore: force https behind proxy for FCC evaluator

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner');
 
 const app = express();
+// Force HTTPS behind proxy (Render/Cloudflare)
+app.set('trust proxy', 1);
+app.use((req, res, next) => {
+  const proto = req.headers['x-forwarded-proto'];
+  if (proto && proto !== 'https') {
+    return res.redirect(301, 'https://' + req.headers.host + req.originalUrl);
+  }
+  next();
+});
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(cors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', allowedHeaders: ['Content-Type'] }));
